41b9ef7bec228a2b2cc72024e06caeecc10d8744
Lenart Svetek <never.saymeme@gmail.com>
2025-01-22 13:43:51 +0100

    ds

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,6 @@ import Map from "./components/Map";
 
 
 export default function Home() {
-  const center = { lat: 40.7128, lng: -74.0060 }; // Example: New York City coordinates
   const zoom = 12;
 
   return (
